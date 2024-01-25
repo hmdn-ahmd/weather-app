f8368f0a3da944ee1445fcff37a3ed532ac5c24a
Ahmed Hamdan <ahmedhamdan2000@gmail.com>
2024-01-25 12:28:11 +0530

    save

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const hbs = require('hbs')
+const path = require("path")
+
+const app = express();
+const weatherData = require("../utils/weatherData");
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req,res) => { 
+    res.send('weather data')
+})
+
+app.get('/weather', (req,res) => {
+    if(!req.query.address) {
+    return res.send('address required')
+    }
+    weatherData(req.query.address,(error,result)=> {
+        if(error) {
+            return res.send(error);
+        }
+        res.send(result)
+    })
+})
+
+app.listen(port, ()=> {
+    console.log('Server port: '+ port)
+})
+
---
+++ utils/weatherData.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+
+const openWeatherApp = {
+    BASE_URL : "https://api.openweathermap.org/data/2.5/weather?q=",
+    SECRET_KEY : "4f46f4842617e0f924da048c7f9fc419"
+}
+
+const  weatherData = (address, callback) => {
+    const url = openWeatherApp.BASE_URL +
+        encodeURIComponent(address) +
+        "&APPID=" +
+        openWeatherApp.SECRET_KEY;
+    console.log("url: ", url);
+    request({ url, json: true }, (error, data) => {
+        if (error) {
+            callback(true, "unable to fetch data," + error);
+        }
+        callback(false, data.body);
+    });
+};
+
+module.exports = weatherData;
